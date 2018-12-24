422de7b454dc42b6921a916b235900a7b0b061c6
Lorefnon <lorefnon@gmail.com>
2018-12-25 01:26:52 +0530

    gh-pages update

---
+++ _next/static/chunks/0.js
@@ -0,0 +1,16 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "../../node_modules/next/dist/client/noop.js":
+/*!*******************************************************************************!*\
+  !*** C:/Users/loref/Projects/greldal-2/node_modules/next/dist/client/noop.js ***!
+  \*******************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=0.js.map
