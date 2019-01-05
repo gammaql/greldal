fca3843e9a94b3c100dddd92bfe6bc1c6c3255d1
Lorefnon <lorefnon@gmail.com>
2019-01-06 00:40:59 +0530

    Restructure deletion operation to use a query based on primary keys
    This fixes issue with knex not supporting limit with delete operation

--- src/DeletionOperationResolver.ts
+++ src/DeletionOperationResolver.ts
@@ -1,9 +1,14 @@
 import { MappedDataSource } from "./MappedDataSource";
 import { OperationResolver } from "./OperationResolver";
-import { pick } from "lodash";
+import { pick, isEqual, uniqWith, compact } from 'lodash';
 import { QueryOperationResolver } from ".";
 import { MemoizeGetter } from "./utils";
 import { OperationMapping } from "./MappedOperation";
+import { MappedQueryOperation } from "./MappedQueryOperation";
+import _debug from "debug"
+import { Dict } from './util-types';
+
+const debug = _debug("greldal:DeletionOperationResolver");
 
 /**
  * Opinionated resolver for deletion of one or more entities from a single data source.
@@ -64,12 +69,36 @@ export class DeletionOperationResolver<
 
     async resolve() {
         const mappedRows = await this.queryResolver.resolve();
-        await this.queryResolver.operation
-            .interceptQueryByArgs(
-                this.rootSource.rootQuery(this.aliasHierarchyVisitor).where(this.storeParams.whereParams),
-                this.args,
-            )
-            .del();
+        const {primaryFields} = this.operation.rootSource;
+        if (primaryFields.length === 0) {
+            throw new Error('DeletionPreset requires some fields to be marked as primary');
+        }
+        const primaryMappers = this.queryResolver.storeParams.primaryMappers.filter((pm) => pm.field.isPrimary);
+        if (primaryMappers.length !== primaryFields.length) {
+            throw new Error('Not all primary keys included in query');
+        }
+        const rows = this.queryResolver.resultRows!
+        const pkVals = uniqWith(compact(rows.map(r => {
+            let queryItem: Dict = {};
+            for (const pm of primaryMappers) {
+                queryItem[pm.field.sourceColumn!] = r[pm.columnAlias!];
+            }
+            return queryItem;
+        })), isEqual);
+        debug("Extracted pkVals %O from %O", pkVals, rows)
+        if (pkVals.length === 0) {
+            throw new Error('Refusing to execute unbounded delete operation');
+        }
+        let queryBuilder = this.rootSource.rootQuery(this.aliasHierarchyVisitor).where(pkVals.shift()!);
+        let whereParam;
+        while (whereParam = pkVals.shift()) {
+            queryBuilder.orWhere(whereParam);
+        }
+        queryBuilder = this.queryResolver.operation.interceptQueryByArgs(
+            queryBuilder,
+            this.args,
+        );
+        await queryBuilder.del();
         return mappedRows;
     }
 }
