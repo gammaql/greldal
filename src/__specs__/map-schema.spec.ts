1212b4483358b19ac9495a885f4b2650db4409ae
Lorefnon <lorefnon@gmail.com>
2019-01-06 21:10:15 +0530

    Add specs for external resolvers and intercepting schema mapping

---
+++ src/__specs__/map-schema.spec.ts
@@ -0,0 +1,47 @@
+import {mapSchema} from "..";
+import { GraphQLString, graphql, GraphQLObjectType, printSchema } from "graphql";
+
+test("External resolver mapping", async () => {
+    const schema = mapSchema([{
+        operationType: "query" as "query",
+        name: "hello",
+        fieldConfig: {
+            type: GraphQLString,
+            resolve() {
+                return "world"
+            }
+        }
+    }]);
+    const r1 = await graphql(
+        schema,
+        `
+            query {
+                hello
+            }
+        `,
+    );
+    expect(r1.errors).not.toBeDefined();
+    expect(r1).toMatchSnapshot();
+})
+
+test("Schema interceptor", () => {
+    const schema = mapSchema([{
+        operationType: "query" as "query",
+        name: "hello",
+        fieldConfig: {
+            type: GraphQLString,
+            resolve() {
+                return "world"
+            }
+        }
+    }], (schemaConfig) => {
+        schemaConfig.subscription = new GraphQLObjectType({
+            name: "Subscription",
+            fields: {
+
+            }
+        });
+        return schemaConfig;
+    }); 
+    expect(printSchema(schema)).toMatchSnapshot();
+})
