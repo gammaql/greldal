169d5fdf58563f2a9b022752493f084ba5129007
Lorefnon <lorefnon@gmail.com>
2019-12-29 22:08:36 +0530

    Improve test coverage of NotificationDispatcher

--- src/predicate.ts
+++ src/predicate.ts
@@ -1,12 +1,14 @@
 import { some } from "lodash";
-import { Predicate, MaybeArray } from "./util-types";
-import { checkStr, checkRegexp, checkArray } from "./guards";
+import { Predicate, MaybeArray, Maybe } from "./util-types";
+import { checkStr, checkRegexp, checkArray, checkNil } from "./guards";
 
-export const matchString = (predicate: MaybeArray<string | RegExp | Predicate<string>>): Predicate<string> =>
+export const matchString = (
+    predicate: MaybeArray<string | RegExp | Predicate<string>>,
+): Predicate<Maybe<string>> =>
     checkArray(predicate)
-        ? (i: string) => some(predicate, p => matchString(p)(i))
+        ? (i: Maybe<string>) => !checkNil(i) && some(predicate, p => matchString(p)(i))
         : checkStr(predicate)
-        ? (i: string) => i == predicate
+        ? (i: Maybe<string>) => i === predicate
         : checkRegexp(predicate)
-        ? (i: string) => !!i.match(predicate)
-        : predicate;
+        ? (i: Maybe<string>) => !checkNil(i) && !! i.match(predicate)
+        : (i: Maybe<string>) => !checkNil(i) && predicate(i);
