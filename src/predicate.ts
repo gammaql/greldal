816739bbf61ca1982c9d936cca9fa506fd308eeb
Lorefnon <lorefnon@gmail.com>
2019-12-29 20:16:58 +0530

    Make notification dispatcher configuration more flexible and add docs

--- src/predicate.ts
+++ src/predicate.ts
@@ -1,9 +1,11 @@
-import * as t from "io-ts";
-import { RegExpType, Predicate } from "./util-types";
-import { checkStr, checkRegexp } from "./guards";
+import { some } from "lodash";
+import { Predicate, MaybeArray } from "./util-types";
+import { checkStr, checkRegexp, checkArray } from "./guards";
 
-export const matchString = (predicate: string | RegExp | Predicate<string>): Predicate<string> =>
-    checkStr(predicate)
+export const matchString = (predicate: MaybeArray<string | RegExp | Predicate<string>>): Predicate<string> =>
+    checkArray(predicate)
+        ? (i: string) => some(predicate, p => matchString(p)(i))
+        : checkStr(predicate)
         ? (i: string) => i == predicate
         : checkRegexp(predicate)
         ? (i: string) => !!i.match(predicate)
